9d0a421697631e5a3ada4eecdcc989e7e63a59e3
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:31:46 +0200

    t

--- api/exercises.js
+++ api/exercises.js
@@ -1,19 +1,12 @@
 import notion, { databaseId } from "../lib/notion";
 
 export default async function handler(req, res) {
-  try {
-    const response = await notion.dataSources.query({
-      data_source_id: databaseId,
-    });
-
-    return res.status(200).json(response.results);
-
-  } catch (error) {
-    console.error(error);
-
-    return res.status(500).json({
-      error: error.message,
-      details: error.body ?? null,
-    });
-  }
+  return res.status(200).json({
+    databaseId,
+    notionExists: !!notion,
+    databasesExists: !!notion.databases,
+    dataSourcesExists: !!notion.dataSources,
+    databasesKeys: notion.databases ? Object.keys(notion.databases) : null,
+    dataSourcesKeys: notion.dataSources ? Object.keys(notion.dataSources) : null,
+  });
 }
