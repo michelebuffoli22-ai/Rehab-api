9d6c9c2fb039629d5a298a741339b8e1cf03534c
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:17:33 +0200

    agggg

--- api/exercises.js
+++ api/exercises.js
@@ -1,9 +1,5 @@
-import notion from "../lib/notion";
-
-export default async function handler(req, res) {
-  return res.status(200).json({
-    keys: Object.keys(notion),
-    databases: typeof notion.databases,
-    dataSources: typeof notion.dataSources,
-  });
-}
+return res.status(200).json({
+  keys: Object.keys(notion),
+  databases: typeof notion.databases,
+  dataSources: typeof notion.dataSources,
+});
