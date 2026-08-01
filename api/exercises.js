654d86a286d12e56785269635a12ef45b7694154
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:18:38 +0200

    Nuovo api exercise js

--- api/exercises.js
+++ api/exercises.js
@@ -1,5 +1,29 @@
-return res.status(200).json({
-  keys: Object.keys(notion),
-  databases: typeof notion.databases,
-  dataSources: typeof notion.dataSources,
-});
+import notion, { databaseId } from "../lib/notion";
+
+export default async function handler(req, res) {
+  try {
+    // Recupera il database (serve per verificare che l'ID sia corretto)
+    const db = await notion.databases.retrieve({
+      database_id: databaseId,
+    });
+
+    // Esegue la query del database
+    const response = await notion.databases.query({
+      database_id: databaseId,
+    });
+
+    return res.status(200).json({
+      database: db.title,
+      count: response.results.length,
+      results: response.results,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      error: error.message,
+      details: error.body ?? null,
+    });
+  }
+}
