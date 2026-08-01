444ec04f2ba2ae76972feb5885c1316e28774250
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:42:58 +0200

    q

--- api/exercises.js
+++ api/exercises.js
@@ -10,52 +10,20 @@ export default async function handler(req, res) {
       const p = page.properties;
 
       return {
-        id: page.id,
-
-        nome:
-          p["Nome Esercizio"]?.title?.[0]?.plain_text || "",
-
-        categoria:
-          p["Categoria"]?.multi_select?.map((x) => x.name) || [],
-
-        distretto:
-          p["Distretto"]?.multi_select?.map((x) => x.name) || [],
-
-        contrazione:
-          p["Contrazione"]?.multi_select?.map((x) => x.name) || [],
-
-        strumenti:
-          p["Strumenti"]?.multi_select?.map((x) => x.name) || [],
-
-        difficolta:
-          p["Difficoltà"]?.select?.name || "",
-
-        serie:
-          p["Serie"]?.rich_text?.[0]?.plain_text || "",
-
-        dosaggio:
-          p["Dosaggio"]?.rich_text?.[0]?.plain_text || "",
-
-        video:
-          p["Video Link"]?.url || "",
-
-        immagine:
-          p["Immagine Url"]?.url || "",
-
-        note:
-          p["Note Cliniche"]?.rich_text?.[0]?.plain_text || ""
+        nome: p["Nome Esercizio"]?.title?.[0]?.plain_text || "",
+        serie: p["Serie"]?.rich_text?.[0]?.plain_text || "",
+        dosaggio: p["Dosaggio"]?.rich_text?.[0]?.plain_text || "",
+        video: p["Video Link"]?.url || "",
       };
     });
 
     res.status(200).json(exercises);
 
   } catch (err) {
-
     console.error(err);
 
     res.status(500).json({
       error: err.message
     });
-
   }
 }
