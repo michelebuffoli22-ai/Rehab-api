42bd8d32a84d04bad466e88c15a5e661c2ac7248
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:12:42 +0200

    agg

--- api/exercises.js
+++ api/exercises.js
@@ -1,16 +1,16 @@
-import notion, { databaseId } from "../lib/notion";
+import notion from "../lib/notion";
 
 export default async function handler(req, res) {
   try {
-    const response = await notion.dataSources.query({
-      data_source_id: databaseId,
+    const pageId = "362fa9c7-3e56-80e3-b4e0-f61ed2b2174d";
+
+    const response = await notion.pages.retrieve({
+      page_id: pageId,
     });
 
-    return res.status(200).json(response.results);
+    return res.status(200).json(response);
 
   } catch (error) {
-    console.error(error);
-
     return res.status(500).json({
       error: error.message,
       details: error.body ?? null,
