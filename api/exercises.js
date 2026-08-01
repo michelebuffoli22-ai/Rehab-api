5b310b8d9ff68f0b711b275cfa149b37a205d1ab
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:23:02 +0200

    Downgrade v notion

--- api/exercises.js
+++ api/exercises.js
@@ -2,22 +2,11 @@ import notion, { databaseId } from "../lib/notion";
 
 export default async function handler(req, res) {
   try {
-    // Recupera il database (serve per verificare che l'ID sia corretto)
-    const db = await notion.databases.retrieve({
-      database_id: databaseId,
-    });
-
-    // Esegue la query del database
     const response = await notion.databases.query({
       database_id: databaseId,
     });
 
-    return res.status(200).json({
-      database: db.title,
-      count: response.results.length,
-      results: response.results,
-    });
-
+    return res.status(200).json(response.results);
   } catch (error) {
     console.error(error);
 
