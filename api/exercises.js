a49e7c2cfb45e37e583dc5c68cb1f984b64c3867
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:30:34 +0200

    corretto data source

--- api/exercises.js
+++ api/exercises.js
@@ -1,17 +1,16 @@
-import notion from "../lib/notion";
+import notion, { databaseId } from "../lib/notion";
 
 export default async function handler(req, res) {
   try {
-    const response = await notion.search({
-      filter: {
-        property: "object",
-        value: "database",
-      },
+    const response = await notion.dataSources.query({
+      data_source_id: databaseId,
     });
 
     return res.status(200).json(response.results);
 
   } catch (error) {
+    console.error(error);
+
     return res.status(500).json({
       error: error.message,
       details: error.body ?? null,
