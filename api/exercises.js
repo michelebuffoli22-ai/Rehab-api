f465f2e8d0bee32993df24b47c84fe30cbeb8f74
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:28:48 +0200

    niti

--- api/exercises.js
+++ api/exercises.js
@@ -1,15 +1,20 @@
-import notion, { databaseId } from "../lib/notion";
+import notion from "../lib/notion";
 
 export default async function handler(req, res) {
+  try {
+    const response = await notion.search({
+      filter: {
+        property: "object",
+        value: "database",
+      },
+    });
 
-  return res.status(200).json({
-    sdk: typeof notion,
-    databases: typeof notion.databases,
-    query: typeof notion.databases?.query,
-    retrieve: typeof notion.databases?.retrieve,
-    dataSources: typeof notion.dataSources,
-    dsQuery: typeof notion.dataSources?.query,
-    databaseId
-  });
+    return res.status(200).json(response.results);
 
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+      details: error.body ?? null,
+    });
+  }
 }
