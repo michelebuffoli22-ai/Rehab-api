e0899e9fd3aa22721c399e96669a6ba9e7949bc1
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:16:05 +0200

    test

--- api/exercises.js
+++ api/exercises.js
@@ -1,19 +1,9 @@
 import notion from "../lib/notion";
 
 export default async function handler(req, res) {
-  try {
-    const pageId = "362fa9c7-3e56-80e3-b4e0-f61ed2b2174d";
-
-    const response = await notion.pages.retrieve({
-      page_id: pageId,
-    });
-
-    return res.status(200).json(response);
-
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-      details: error.body ?? null,
-    });
-  }
+  return res.status(200).json({
+    keys: Object.keys(notion),
+    databases: typeof notion.databases,
+    dataSources: typeof notion.dataSources,
+  });
 }
