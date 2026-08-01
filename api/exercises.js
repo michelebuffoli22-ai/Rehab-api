29b83f98c5ef37497a64af78bb0c46e2602d2ce2
Michele Buffoli <michele.buffoli@gmail.com>
2026-08-01 15:39:07 +0200

    Aggiorn

--- api/exercises.js
+++ api/exercises.js
@@ -6,14 +6,53 @@ export default async function handler(req, res) {
       database_id: databaseId,
     });
 
-    return res.status(200).json(response.results);
+    const exercises = response.results.map(page => {
+      const p = page.properties;
 
-  } catch (error) {
-    console.error(error);
+      return {
+        id: page.id,
 
-    return res.status(500).json({
-      error: error.message,
-      details: error.body ?? null,
+        nome:
+          p["Nome Esercizio"]?.title?.[0]?.plain_text ?? "",
+
+        categoria:
+          p["Categoria"]?.multi_select?.map(x => x.name) ?? [],
+
+        distretto:
+          p["Distretto"]?.multi_select?.map(x => x.name) ?? [],
+
+        strumenti:
+          p["Strumenti"]?.multi_select?.map(x => x.name) ?? [],
+
+        difficolta:
+          p["Difficoltà"]?.select?.name ?? "",
+
+        serie:
+          p["Serie"]?.rich_text?.[0]?.plain_text ?? "",
+
+        dosaggio:
+          p["Dosaggio"]?.rich_text?.[0]?.plain_text ?? "",
+
+        video:
+          p["Video Link"]?.url ?? "",
+
+        immagine:
+          p["Immagine Url"]?.url ?? "",
+
+        note:
+          p["Note Cliniche"]?.rich_text?.[0]?.plain_text ?? ""
+      };
     });
+
+    res.status(200).json(exercises);
+
+  } catch (err) {
+
+    console.error(err);
+
+    res.status(500).json({
+      error: err.message
+    });
+
   }
 }
